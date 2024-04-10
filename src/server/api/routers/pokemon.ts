ff5c43bb811273059b8185af389b61e430c0ca4e
Jose Miguel Valiente <josemiguel.valiente@star-robotics.com>
2024-04-10 23:01:44 +0200

    feat: Update Pokemon API integration to limit results to 20 and add byId endpoint

--- src/server/api/routers/pokemon.ts
+++ src/server/api/routers/pokemon.ts
@@ -1,10 +1,17 @@
+import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 const base = process.env.API_BASE;
 
 export const pokemonRouter = createTRPCRouter({
   all: publicProcedure.query(async () => {
-    const data = await fetch(`${base}/pokemon?limit=10000`);
+    const data = await fetch(`${base}/pokemon?limit=20`);
     return data.json();
   }),
+  byId: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ input }) => {
+      const data = await fetch(`${base}/pokemon/${input.id}`);
+      return data.json();
+    }),
 });
