f016ef02700d2ebe6123f741ec608940936e64cb
Jose Miguel Valiente <josemiguel.valiente@star-robotics.com>
2024-04-11 17:33:27 +0200

    feat: Add Pagination component to Home page

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,14 @@
+import ButtonContainer from "~/ui/Button/ButtonContainer";
+import SearchBarContainer from "~/ui/SearchBar/SearchBarContainer";
+
+const Pagination = () => {
+  return (
+    <div className="inline-flex gap-2">
+      <SearchBarContainer />
+      <ButtonContainer name="Prev" />
+      <ButtonContainer name="Next" />
+    </div>
+  );
+};
+
+export default Pagination;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Pagination from "~/components/Pagination/Pagination";
 import PokemonList from "~/components/PokemonList/PokemonList";
 import type { PokemonResponse } from "~/types/pokemon";
 
@@ -13,7 +14,10 @@ export default function Home() {
         <title>Pokemon App Binpar</title>
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div className=" grid grid-cols-1 gap-4 p-10 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-8">
+        <div className="p-10">
+          <Pagination />
+        </div>
+        <div className="grid grid-cols-1 gap-4 p-5 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-8">
           <PokemonList pokemons={pokemons} />;
         </div>
       </main>
