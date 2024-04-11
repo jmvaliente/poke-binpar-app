dd07febf84c4c2ce3a48bb270116135e0685ddc3
Jose Miguel Valiente <josemiguel.valiente@star-robotics.com>
2024-04-11 13:08:15 +0200

    UI: Update grid layout in Home component to be responsive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
         <title>Pokemon App Binpar</title>
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div className="grid grid-cols-8 gap-4 p-10">
+        <div className=" grid grid-cols-1 gap-4 p-10 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-8">
           <PokemonList pokemons={pokemons} />;
         </div>
       </main>
