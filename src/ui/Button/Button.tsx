ee42cf8affce3b1ed9e4a9f89a466eea9779818b
Jose Miguel Valiente <josemiguel.valiente@star-robotics.com>
2024-04-13 20:04:32 +0200

    UI: Update Button component to include disabled prop and opacity-50 style

--- src/ui/Button/Button.tsx
+++ src/ui/Button/Button.tsx
@@ -6,7 +6,7 @@ const Button = ({ children, handleClick, disabled }: ButtonProps) => {
     <button
       disabled={disabled} // Explicitly specify the type as boolean
       onClick={handleClick}
-      className={`${isNext && "rounded-r"} bg-gray-300 px-4 py-2 font-bold text-gray-800 hover:bg-gray-400`}
+      className={`${isNext && "rounded-r"}  bg-gray-300 px-4 py-2 font-bold text-gray-800 hover:bg-gray-400 disabled:opacity-50`}
     >
       {children}
     </button>
