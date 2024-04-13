7abcc2709720a27342d0209f2606c276e0f01d07
Jose Miguel Valiente <josemiguel.valiente@star-robotics.com>
2024-04-13 20:05:49 +0200

    Add search functionality to PokemonStore

--- src/store/pokemonStore.ts
+++ src/store/pokemonStore.ts
@@ -5,9 +5,11 @@ interface PokemonStore {
   url: string;
   next: string | null;
   previous: string | null;
+  search: string;
   updateUrl: (url: string) => void;
   updatePrevious: (previous: string) => void;
   updateNext: (next: string) => void;
+  updateSearch: (search: string) => void;
 }
 
 export const useStore = create<PokemonStore>((set) => ({
@@ -16,7 +18,9 @@ export const useStore = create<PokemonStore>((set) => ({
   url: "",
   next: null,
   previous: null,
+  search: "",
   updateUrl: (url) => set((state) => ({ ...state, url })),
   updatePrevious: (previous) => set((state) => ({ ...state, previous })),
   updateNext: (next) => set((state) => ({ ...state, next })),
+  updateSearch: (search) => set((state) => ({ ...state, search })),
 }));
