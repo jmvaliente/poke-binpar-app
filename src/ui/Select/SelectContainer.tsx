53d4acb89302e2c7dfb1b6311ee5d095e3c301aa
Jose Miguel Valiente <josemiguel.valiente@star-robotics.com>
2024-04-14 23:01:19 +0200

    Refactor SelectContainer component to use tailwind CSS classes in SelectContainer.tsx

--- src/ui/Select/SelectContainer.tsx
+++ src/ui/Select/SelectContainer.tsx
@@ -11,13 +11,14 @@ const SelectContainer = ({ data }: SelectContainerProps) => {
     e.target.options[e.target.selectedIndex]?.text ?? "";
 
   return (
-    <div>
+    <div className="relative h-10 w-72 min-w-[200px]">
       <select
+        className="border-blue-gray-200 text-blue-gray-700 placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 disabled:bg-blue-gray-50 placeholder-shown:borde peer h-full w-full rounded-[7px] border border-t-transparent bg-transparent bg-white px-3 py-2.5 font-sans text-sm font-normal outline outline-0 transition-all focus:border-gray-900 "
         onChange={(event) =>
           handleChangeFilter(event.target.value, extractOptionName(event))
         }
       >
-        <option>All</option>
+        <option value={""}>All</option>
         {data.results.map((data) => {
           const isSelected = storage.filter === data.name;
           return <Select key={data.name} data={data} selected={isSelected} />;
