aca8a44c6b85235bf6ef203071efbad66ef4f397
Jose Miguel Valiente <josemiguel.valiente@star-robotics.com>
2024-04-17 22:21:08 +0200

    Refactor filter components to handle filter types and filter generation

--- src/components/Filter/FilterType.tsx
+++ src/components/Filter/FilterType.tsx
@@ -5,7 +5,9 @@ import { api } from "~/utils/api";
 const FilterType = () => {
   const types = api.type.all.useQuery();
   if (!types.isSuccess) return null;
-  return <SelectContainer data={types.data as GenerationResults} />;
+  return (
+    <SelectContainer data={types.data as GenerationResults} filterType="type" />
+  );
 };
 
 export default FilterType;
